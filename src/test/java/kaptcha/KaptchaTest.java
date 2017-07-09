a42734cd8e69e77b02561b09a5e94aa07b670b26
lusheng0596 <523402904@qq.com>
2017-07-09 23:12:44 +0800

    update

--- src/test/java/kaptcha/KaptchaTest.java
+++ src/test/java/kaptcha/KaptchaTest.java
@@ -1,10 +1,23 @@
 package kaptcha;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import kaptcha.service.BufferedImageService;
+import kaptcha.vo.KeyBufferImage;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 //@SpringApplicationConfiguration(classes = KaptchaApplication.class)
+@SpringBootTest(classes = KaptchaApplication.class)
 public class KaptchaTest {
-
+	@Autowired
+	BufferedImageService service;
+	@Test
+	public void test() {
+		KeyBufferImage oneImage = service.getOneImage();
+		System.out.println(oneImage.getKey());
+	}
 }
